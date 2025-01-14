dc7374a34affe8f8e42881d143270b155cb966b1
Enrique Garcia <enriquegc.1991@gmail.com>
2025-01-14 13:49:14 +0100

    Correcciones

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-
 import { HeroComponent } from "./components/hero/hero.component";
 import { ListComponent } from "./components/list/list.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [
@@ -14,7 +13,7 @@ import { ListComponent } from "./components/list/list.component";
         ListComponent
     ],
     imports: [
-        CommonModule,
+        CommonModule
     ]
 })
 export class HeroesModule {
